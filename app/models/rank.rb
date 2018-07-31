5a51b7f39cd7d2695abe2dbdc60bb4ac55771a04
Dan Chung <danetix@gmail.com>
2018-07-31 14:35:29 -0400

    reverted relationship change

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,4 +1,4 @@
 class Rank < ApplicationRecord
-  belongs_to :ranker, :class_name => 'User', :inverse_of => :ranks
+  belongs_to :ranker, :class_name => 'User', :foreign_key => :ranker_id, :inverse_of => :ranks
   belongs_to :rankee, :class_name => 'User'
 end
