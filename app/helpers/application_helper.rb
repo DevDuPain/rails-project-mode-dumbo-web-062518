802b6ad403c40708b39a7afa90da9eec3e44a3ca
alexneustein <alex.neustein@gmail.com>
2018-08-03 11:07:45 -0400

    Added front page stock photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def randomized_background_image
+    images = ["./assets/stockphotos/image1.jpg",
+      "./assets/stockphotos/image2.jpg",
+      "./assets/stockphotos/image3.jpg",
+      "./assets/stockphotos/image4.jpg",
+      "./assets/stockphotos/image5.jpg",
+      "./assets/stockphotos/image6.jpg"
+    ]
+    images[rand(images.size)]
+  end
 end
